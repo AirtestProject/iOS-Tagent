c7d3a7feab770d054d0fb5c663eb178b6ccf8a90
Mykola Mokhnach <mokhnach@gmail.com>
2019-08-29 11:08:14 +0200

    fix: Tune the condition for input preparation

--- WebDriverAgentLib/Categories/XCUIElement+FBTyping.m
+++ WebDriverAgentLib/Categories/XCUIElement+FBTyping.m
@@ -27,8 +27,7 @@
   
   // Sometimes the keyboard is not opened after the first tap, so we need to retry
   for (int tryNum = 0; tryNum < 2; ++tryNum) {
-    [self fb_tapWithError:error];
-    if (isKeyboardAlreadyVisible) {
+    if ([self fb_tapWithError:error] && isKeyboardAlreadyVisible) {
       return YES;
     }
     [self fb_waitUntilSnapshotIsStable];
