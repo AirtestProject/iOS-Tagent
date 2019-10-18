abb752ae8c04fd9155823c24f9fc9ed431d648e2
Isaac A. Murchie <isaac.appium@gmail.com>
2019-10-18 11:42:33 -0400

    refactor: address comments on #238 (#242)

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ import * as proxies from './lib/no-session-proxy';
 import * as driver from './lib/webdriveragent';
 import * as constants from './lib/constants';
 import * as utils from './lib/utils';
+import { asyncify } from 'asyncbox';
 
 
-const { checkForDependencies, retrieveBuildDir, bundleWDASim } = dependencies;
+const { checkForDependencies, bundleWDASim } = dependencies;
 const { NoSessionProxy } = proxies;
 const { WebDriverAgent } = driver;
 const { WDA_BUNDLE_ID, BOOTSTRAP_PATH, WDA_BASE_URL, WDA_RUNNER_BUNDLE_ID, PROJECT_FILE } = constants;
 const { resetTestProcesses } = utils;
 
 
+// When run as a command line utility, this should check for the dependencies
+if (require.main === module) {
+  asyncify(checkForDependencies);
+}
+
 export {
   WebDriverAgent,
   NoSessionProxy,
-  checkForDependencies, retrieveBuildDir, bundleWDASim,
+  checkForDependencies, bundleWDASim,
   resetTestProcesses,
   BOOTSTRAP_PATH, WDA_BUNDLE_ID,
   WDA_RUNNER_BUNDLE_ID, PROJECT_FILE,
