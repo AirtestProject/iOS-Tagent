4b3c220c0c609802924b7b6ff9a4dfa7a98eb5f4
Mykola Mokhnach <mokhnach@gmail.com>
2024-04-07 21:48:06 +0200

    feat: Add types for WDA caps and settings (#885)

---
+++ index.ts
@@ -0,0 +1,7 @@
+export { checkForDependencies, bundleWDASim } from './lib/check-dependencies';
+export { NoSessionProxy } from './lib/no-session-proxy';
+export { WebDriverAgent } from './lib/webdriveragent';
+export { WDA_BASE_URL, WDA_RUNNER_BUNDLE_ID, PROJECT_FILE } from './lib/constants';
+export { resetTestProcesses, BOOTSTRAP_PATH } from './lib/utils';
+
+export * from './lib/types';
---
+++ lib/types.ts
@@ -0,0 +1,50 @@
+// WebDriverAgentLib/Utilities/FBSettings.h
+export interface WDASettings {
+  elementResponseAttribute?: string;
+  shouldUseCompactResponses?: boolean;
+  mjpegServerScreenshotQuality?: number;
+  mjpegServerFramerate?: number;
+  screenshotQuality?: number;
+  elementResponseAttributes?: string;
+  mjpegScalingFactor?: number;
+  mjpegFixOrientation?: boolean;
+  keyboardAutocorrection?: boolean;
+  keyboardPrediction?: boolean;
+  customSnapshotTimeout?: number;
+  snapshotMaxDepth?: number;
+  useFirstMatch?: boolean;
+  boundElementsByIndex?: boolean;
+  reduceMotion?: boolean;
+  defaultActiveApplication?: string;
+  activeAppDetectionPoint?: string;
+  includeNonModalElements?: boolean;
+  defaultAlertAction?: 'accept' | 'dismiss';
+  acceptAlertButtonSelector?: string;
+  dismissAlertButtonSelector?: string;
+  screenshotOrientation?: 'auto' | 'portrait' | 'portraitUpsideDown' | 'landscapeRight' | 'landscapeLeft'
+  waitForIdleTimeout?: number;
+  animationCoolOffTimeout?: number;
+}
+
+// WebDriverAgentLib/Utilities/FBCapabilities.h
+export interface WDACapabilities {
+  bundleId?: string;
+  initialUrl?: string;
+  arguments?: string[];
+  environment?: Record<string, string>;
+  eventloopIdleDelaySec?: number;
+  shouldWaitForQuiescence?: boolean;
+  shouldUseTestManagerForVisibilityDetection?: boolean;
+  maxTypingFrequency?: number;
+  shouldUseSingletonTestManager?: boolean;
+  waitForIdleTimeout?: number;
+  shouldUseCompactResponses?: number;
+  elementResponseFields?: unknown;
+  disableAutomaticScreenshots?: boolean;
+  shouldTerminateApp?: boolean;
+  forceAppLaunch?: boolean;
+  useNativeCachingStrategy?: boolean;
+  forceSimulatorSoftwareKeyboardPresence?: boolean;
+  defaultAlertAction?: 'accept' | 'dismiss';
+  appLaunchStateTimeoutSec?: number;
+}
