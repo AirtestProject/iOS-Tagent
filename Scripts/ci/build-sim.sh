d2bbcc6d7af6b8eea076e24cd18429b74eeaffd6
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-06-23 19:38:22 -0700

    chore: keep entire app for simulators

--- Scripts/ci/build-sim.sh
+++ Scripts/ci/build-sim.sh
@@ -9,11 +9,7 @@ xcodebuild clean build-for-testing \
 
 pushd $WD
 
-# The reason why here excludes several frameworks are:
-# - Xcode 16 started generating 5.9MB of 'Testing.framework', but it might not be necessary for WDA.
-# - libXCTestSwiftSupport is used for Swift testing. WDA doesn't include Swift stuff, thus this is not needed.
-zip -r $ZIP_PKG_NAME $SCHEME-Runner.app \
-    -x "$SCHEME-Runner.app/Frameworks/Testing.framework*" \
-       "$SCHEME-Runner.app/Frameworks/libXCTestSwiftSupport.dylib"
+# Simulators might have an issue to lauch if we drop frameworks even we don't use them.
+zip -r $ZIP_PKG_NAME $SCHEME-Runner.app
 popd
 mv $WD/$ZIP_PKG_NAME ./
